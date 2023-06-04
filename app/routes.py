1c39c682a3f011d3633566615626ca41c3d90dd4
L0n35 <lachlanrjones@gmail.com>
2023-06-04 19:42:58 +0800

    rara

--- app/routes.py
+++ app/routes.py
@@ -200,7 +200,9 @@ def Sort(sub_li):
     # reverse = None (Sorts in Ascending order)
     # key is set to sort using second element of
     # sublist lambda has been used
-    return(sorted(sub_li, key=lambda x: x[3],reverse=True))
+
+    if (lambda x: x[3] != None):
+        return(sorted(sub_li, key=lambda x: x[3],reverse=True))
 
 @app.route('/dash')
 def dash():
